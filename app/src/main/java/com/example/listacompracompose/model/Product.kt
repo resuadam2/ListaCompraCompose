9e863ac4e9578334f98b74ecb4e2d4eca07c75e3
resuadam2 <resuadam2@gmail.com>
2024-04-10 14:34:41 +0200

    first try 24-04-10

--- app/src/main/java/com/example/listacompracompose/model/Product.kt
+++ app/src/main/java/com/example/listacompracompose/model/Product.kt
@@ -1,6 +1,11 @@
 package com.example.listacompracompose.model
 
-class Product (val name: String, var checked: Boolean = false) {
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+
+class Product (val name: String) {
+    var checked by mutableStateOf(false)
     fun toggleChecked() { checked = !checked }
 }
 
